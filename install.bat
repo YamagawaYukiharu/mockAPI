9738ddaa6fe0e6a5e46af418d8a242963d1d6f89
ryamagawa <ryamagawa@localhost.localdomain>
2025-02-19 23:51:12 -0300

    Added installers for windows and linux

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Installing required dependencies, wait a moment...
+pip install -r requirements.txt
+
+echo Installation completed, enjoy!!!!
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing required dependencies for mockPIE, wait a moment..."
+pip install -r requirements.txt
+
+echo "Installation completed, enjoy!!!!"
